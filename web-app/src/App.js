ad2899a9d32f1047813a521d7f9a120973e37e21
Carlos12001 <62664256+Carlos12001@users.noreply.github.com>
2022-05-05 18:43:36 -0600

    Web with date

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,24 +1,71 @@
 import { useState } from "react";
-import "./App.css";
-import { fetchHelloWorld } from "./services/HelloWorldService";
+import React from 'react';
+import './App.css';
+import { fetchHelloWorld } from './services/HelloWorldService';
 
-//Crea el boton
-function App() {
-  //Asi es como se crea una variable en react
-  const [label, setLabel] = useState(""); //label es el nombre de la variable, y setLabel es para setear el valor
+// //Crea el boton
+// function App() {
+//   //Asi es como se crea una variable en react
+//   const [label, setLabel] = useState(""); //label es el nombre de la variable, y setLabel es para setear el valor
 
-  //Esta funcionn es handler del boton que espera el output del fetchHelloWorld y cuando lo recibe setea en label
-  const clickHandler = async () => {
+//   //Esta funcionn es handler del boton que espera el output del fetchHelloWorld y cuando lo recibe setea en label
+//   const clickHandler = async () => {
+//     const result = await fetchHelloWorld();
+//     setLabel(result);
+//   };
+
+//   //Esto ya es HTML, se crea el boton en la ventana e imprime el label
+//   return (
+//     <div className="App">
+//       <button onClick={() => clickHandler()}>Call API</button>
+//       {label}
+//     </div>
+//   );
+// }
+// export default App;
+
+
+
+export class App extends React.Component {
+  constructor() {
+    super();
+
+    //Create atribute currentTime
+    this.state = {
+      currentTime: Date.now()
+    }
+    
+    //Create atribute label
+    this.state = {
+      label: ""
+    }
+
+    //Update all the date in 1s
+    setInterval(() => {
+      this.setState({
+        currentTime: Date.now()
+      });
+    }, 1000)
+  }
+
+  //Es aqui donde se vuelve asyncrono funciona hasta que se pueda ejecutar todo el codigo
+  async clickHandler(){
+    //Aqui el codigo si detiene
     const result = await fetchHelloWorld();
-    setLabel(result);
-  };
+    this.setState({
+      label:result
+    });
+  }
 
-  //Esto ya es HTML, se crea el boton en la ventana e imprime el label
-  return (
-    <div className="App">
-      <button onClick={() => clickHandler()}>Call API</button>
-      {label}
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        {new Date(this.state.currentTime).toLocaleString()}
+        <button onClick={() => this.clickHandler()}>Call API</button>
+        {this.state.label}
+      </div>
+    );
+  }
 }
+
 export default App;
